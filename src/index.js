44403ada22a091415a2856ee2813d3693142f37d
rl-king <hello@rubenlieking.com>
2017-09-24 18:29:47 +0200

    Add firebase sdk

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
 require('style.scss');
 require('register-sw.js');
+import Firebase from 'firebase/app';
+import 'firebase/database';
 
+var config = {
+    databaseURL: 'https://hacker-news.firebaseio.com'
+};
+
+Firebase.initializeApp(config);
 
 var Elm = require('Main');
 var app = Elm.Main.fullscreen();
+var api = Firebase.database().ref("v0");
 
 window.addEventListener("load", function() {
-    document.querySelector(".notification").remove()
-})
+    document.querySelector(".notification").remove();
+
+});
+
+function getPage(title) {
+    return api.child(title).once("value", function(snapshot) {
+        return snapshot.val();
+    });
+}
+
+function getItem(id) {
+    return api.child(id).on("value", function(snapshot) {
+        return snapshot.val();
+    });
+}
